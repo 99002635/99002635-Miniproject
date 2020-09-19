172370ae2abf2ed9b955c7bd38a4ed3a57554cdf
99002635 <71486865+99002635@users.noreply.github.com>
2020-09-19 13:01:17 +0530

    Update operations.h

--- 3. IMPLEMENTATION/inc/operations.h
+++ 3. IMPLEMENTATION/inc/operations.h
@@ -1,1 +1,11 @@
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
 
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+
+#endif  
